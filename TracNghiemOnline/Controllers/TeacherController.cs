b8a454490083a452ba9bc7be29c8ff24231089c6
Dzu <dzu6996@gmail.com>
2018-10-27 17:00:02 +0700

    Done Teacher Modul

--- TracNghiemOnline/Controllers/TeacherController.cs
+++ TracNghiemOnline/Controllers/TeacherController.cs
@@ -4,22 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TracNghiemOnline.Common;
+using TracNghiemOnline.Models;
 namespace TracNghiemOnline.Controllers
 {
     public class TeacherController : Controller
     {
         User user = new User();
+        TeacherDA Model = new TeacherDA();
         // GET: Teacher
         public ActionResult Index()
         {
-            if (!user.IsLogin())
-                return RedirectToAction("Index", "Login");
-            return View();
+            if (!user.IsTeacher())
+                return View("Error");
+            Model.UpdateLastLogin();
+            Model.UpdateLastSeen("Trang Chủ", Url.Action("Index"));
+            return View(Model.GetListTest());
+        }
+        public ActionResult Preview(int id)
+        {
+            if (!user.IsTeacher())
+                return View("Error");
+            var list = Model.GetListScore(id);
+            ViewBag.test_code = id;
+            ViewBag.total = list.Count;
+            return View(list);
         }
         public ActionResult Logout()
         {
-            //Common.UserSession.RemoveSession("User");
-            //Common.UserSession.RemoveSession("Permission");
+            if (!user.IsTeacher())
+                return View("Error");
             user.Reset();
             return RedirectToAction("Index", "Login");
         }
